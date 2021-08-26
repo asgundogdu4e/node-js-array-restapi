087383e1e28ba416e069ceea41e0333972908770
Abdülselam GÜNDOĞDU <asgundogdu@ailevecalisma.gov.tr>
2021-08-26 15:01:16 +0300

    Header

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, token');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  console.log(res.header);
   next();
 });
 
